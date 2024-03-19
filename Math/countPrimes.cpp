0678690690b928a583c3003cc5eb2dfe5f44c4d4
Nidhi Charate <nchat2002@gmail.com>
2024-03-19 16:26:57 +0530

    Update countPrimes.cpp

--- Math/countPrimes.cpp
+++ Math/countPrimes.cpp
@@ -5,7 +5,7 @@ Output: 4
 Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 Edge case if n<2 return 0;
 */
-
+//    TC : O(n*log(log(n)))     SC:O(N)
 //SOLN 
 class Solution {
 public:
